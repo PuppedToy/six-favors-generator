1a4cfdb87261f4a928a90fcd13cad458719b2c29
PuppedToy <alcazar.sacris@gmail.com>
2023-03-30 13:27:09 +0200

    Card cost

--- index.js
+++ index.js
@@ -1,26 +1,76 @@
 const sharp = require('sharp');
+const path = require('path');
 
-function getTextBuffer(text) {
+const COST_FONT = path.join(__dirname, 'fonts', 'MedievalSharp-Regular.ttf');
+
+const lords = {
+  gaidda: {
+    costColor: '#B1CAB1',
+  },
+  noumi: {
+    costColor: '#FBF8D9',
+  },
+  ysault: {
+    costColor: '#FBD9D9',
+  },
+  seraneri: {
+    costColor: '#D9F8FB',
+  },
+  boumandow: {
+    costColor: 'white',
+  },
+  hesif: {
+    costColor: '#D7B5FC',
+  },
+};
+
+function firstLetterToUpperCase(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function getCardFramePath(lord, rarity) {
+  return `./frames/${firstLetterToUpperCase(lord)}${firstLetterToUpperCase(rarity)}.png`;
+}
+
+function getTextBuffer(text, lord) {
     // Calculate the position and size of the text overlay
     const textWidth = 20;
     const textHeight = 20;
   
     // Create an SVG element
-    const svg = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="${textWidth}" height="${textHeight}">
-        <text x="0" y="${textHeight / 2}" fill="white" font-family="Arial" font-size="10" font-weight="bold">${text}</text>
-      </svg>
-    `;
+    const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="${textWidth}" height="${textHeight}">
+      <style>
+        @font-face {
+          font-family: 'MedievalSharp';
+          src: url('fonts/MedievalSharp.eot');
+          src: url('fonts/MedievalSharp.eot?#iefix') format('embedded-opentype'),
+              url('fonts/MedievalSharp.woff2') format('woff2'),
+              url('fonts/MedievalSharp.woff') format('woff'),
+              url('fonts/MedievalSharp.ttf') format('truetype'),
+              url('fonts/MedievalSharp.svg#MedievalSharp') format('svg');
+          font-weight: 500;
+          font-style: normal;
+          font-display: swap;
+        }   
+        text {
+          font-family: 'MedievalSharp';
+          font-size: 10px;
+          fill: ${lords[lord].costColor}};
+        }
+      </style>
+      <text x="0" y="${textHeight / 2}">${text}</text>
+    </svg>`;
   
     // Convert the SVG to a buffer that sharp can use
     const textImageBuffer = Buffer.from(svg);
     return sharp(textImageBuffer, { density: 300 })
       .png()
       .toBuffer();
-  }
+}
 
-async function generateCard(cardFramePath, cardImagePath, text, outputPath) {
+async function generateCard(lord, rarity, cardImagePath, text, outputPath) {
   try {
+    const cardFramePath = getCardFramePath(lord, rarity);
     const { width, height } = await sharp(cardFramePath).metadata();
     
     // Calculate position and size of the overlay
@@ -40,9 +90,9 @@ async function generateCard(cardFramePath, cardImagePath, text, outputPath) {
       .toBuffer();
 
     const textWidth = 25;
-    const textTop = 5;
-    const textLeft = width - textWidth - 20;
-    const textImage = getTextBuffer(text);
+    const textTop = 2;
+    const textLeft = width - textWidth - 16;
+    const textImage = getTextBuffer(text, lord);
 
     const [frameBuffer, imageBuffer, textBuffer] = await Promise.all([frameResizer, imageResizer, textImage]);
     
@@ -67,7 +117,13 @@ async function generateCard(cardFramePath, cardImagePath, text, outputPath) {
   }
 }
 
-const CARD_FRAME_PATH = './tests/CardFrame.png';
 const CARD_IMAGE_PATH = './tests/CardImage.png';
-const OUTPUT_PATH = './tests/output.png';
-generateCard(CARD_FRAME_PATH, CARD_IMAGE_PATH, '1', OUTPUT_PATH);
+const OUTPUT_PATH = 'output.png';
+
+const rarities = ['common', 'rare', 'epic', 'legendary'];
+
+Object.keys(lords).forEach((lord) => {
+  rarities.forEach((rarity) => {
+    generateCard(lord, rarity, CARD_IMAGE_PATH, '2', `./tests/out/${lord}-${rarity}.png`);
+  });
+});
